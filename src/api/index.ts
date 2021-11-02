0c77135cc7709af4fc7a67eef42dde824f1c54c0
JuyeongByeon <wndudqus@naver.com>
2021-11-03 00:00:55 +0900

    config dome

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,8 +10,8 @@ router.get('/', (req, res, next) => {});
 //api response
 router.get('/:id', (req, res, next) => {
 	const id = req.params.id;
-	dbConnection.connect('');
-	dbConnection.query(`SELECT password from Users where id="${id}";`, (error, rows, filds) => {
+	dbConnection.connect();
+	dbConnection.query(`SELECT password from Users where id=${id}`, (error, rows, filds) => {
 		res.send({
 			rows,
 		});
